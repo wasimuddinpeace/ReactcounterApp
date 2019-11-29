a2cf4a28020adaf16d34ab561e86bcf0155473db
Waseem Uddin <waseemsde@gmail.com>
2019-11-28 17:11:55 -0800

    Embedding Expression Object Destructuring  Setting Attributes Rendering Lists(Map function) and Classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';  
 class Counter extends Component {
+
+    state={
+          count:0,
+          tags:['tag1','tag2','tag3']
+    };
+
+    styles={
+      fontSize:30,
+      fontWeight:'bold'
+    };
     
+    
+    //embeding xpresiion
+    //Object Destructuring
     render() { 
-        return <h1>Hello waseem Uddin Hammad</h1>
+        return (
+        <div>
+            <span   style={{fontSize:30}}  className={this.getColourForclasses()}>{this.getCountFromState()}</span>
+            <button style={this.styles} className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+               {this.state.tags
+                .map(tag=><li key={tag}>{tag}</li>)
+                }
+            </ul>
+            </div>
+            );
+    
     }
+    getCountFromState(){
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count 
+    }
+
+    getColourForclasses(){
+    let classes="badge m-2 badge-"
+    classes+= this.state.count===0? "warning": "primary"
+    return classes;
+}
+
 }
  
 export default Counter ;
